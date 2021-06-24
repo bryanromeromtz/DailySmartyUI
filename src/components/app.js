08764e2d31d1e845df20234062392b62c433ecc4
bryanromeromtz <cryptomtz@hotmail.com>
2021-06-24 01:12:30 -0500

    created recent post and search bar components

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
 import Logo from "./logo";
+import SearchBar from "./searchBar";
+import RecentPost from "./recentPosts";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <Logo />
+        <SearchBar />
+        <RecentPost />
       </div>
     );
   }
