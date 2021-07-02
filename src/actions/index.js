66f287267337f146a2a80c93981644bab0729642
bryanromeromtz <cryptomtz@hotmail.com>
2021-07-01 22:28:02 -0500

    modified SearchBar component to take onSubmit function via props

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,6 @@ export function fetchRecentPosts() {
   return function (dispatch) {
     axios.get('https://api.dailysmarty.com/posts')
       .then(response => {
-        console.log('response fetch', response);
-
         dispatch({
           type: SET_RECENT_POSTS,
           payload: response.data.posts
--- src/components/results.js
+++ src/components/results.js
@@ -4,11 +4,17 @@ import Logo from './logo';
 import SearchBar from './searchBar';
 
 export default class Results extends Component {
+
+  handleSearchBarSubmit(query) {
+    console.log('handle search bar submit', query);
+
+  }
+
   render() {
     return (
       <div>
         <Logo size={55} />
-        <SearchBar />
+        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
       </div>
     )
   }
